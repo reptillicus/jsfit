65403519dece346612e475f5a94df01619bf2fd2
jmeiring <josephmeiring@gmail.com>
2014-04-24 15:27:02 -0500

    what

--- src/minimizer.js
+++ src/minimizer.js
@@ -2,80 +2,140 @@
 
 function Minimizer(model, data, initialParams, options) {
   var self = this;
-  self.epsilon = numeric.epsilon;
+  self.epsilon = numeric.epsilon*100;
   self.xvals = data[0];
   self.yvals = data[1];
   self.model = model;
   self.params = initialParams;
   self.initialParams = initialParams;
   self.npars = initialParams.length;
-  console.log(self)
 
   var defaultOptions = {
-    maxIterations: 100
+    maxIterations: 100, 
+    debug: false,
+    ftol :1e-10
   };
 
-  var fitterOptions = defaultOptions;
+  self.fitterOptions = defaultOptions;
   for (var key in options) {
     if (options.hasOwnProperty(key)) {
-      if (options[key] !== undefined) fitterOptions[key] = options[key];
+      if (options[key] !== undefined) self.fitterOptions[key] = options[key];
+    }
+  }
+
+  //Make sure that parInfo, if it cam through, is the same length as the 
+  //parameter array
+  if (self.fitterOptions.parInfo) {
+    if (self.fitterOptions.parInfo.length !== self.npars) {
+      throw new Error('parInfo and params must be SAME length')
     }
   }
 
   this.residuals = function(params) {
     var resid =[];
     for (var i=0; i<self.xvals.length; i++) {
-      val = Math.pow(self.yvals[i] - self.model(self.xvals[i], params), 2);
+      var val = Math.pow(self.yvals[i] - self.model(self.xvals[i], params), 2);
       resid.push(val);
     }
     return resid;
   };
 
-  this.jacobian = function(params) {
-    var h, 
-        fjac = numeric.rep([self.npars,self.xvals.length],0),
-        upper, lower, 
-        upper_params = [], 
-        lower_params = [];
-    console.log(upper_params, lower_params)
-    for (var i=0; i<params.length; i++) {
-      h = params[i] * self.epsilon;
-      params[i] = params[i] + h
-      for (var j=0; j<self.xvals.length; j++) {
-        // upper = self.model(self.xvals[j], upper_params);
-        // lower = self.model(self.xvals[j], lower_params);
-        var val = self.model(self.xvals[j], params);
-        // console.log(upper, lower, h)
-        // fjac[i][j] = 0.5*(upper-lower) / h;
-        fjac[i][j] = 1.0
+  this.ssr = function(params) {
+    var ssr;
+    ssr = numeric.sum(self.residuals(params));
+    return ssr;
+  };
+
+  this.diagonal = function (arr) {
+    var dim, out;
+    dim = numeric.dim(arr);
+    out = numeric.rep(dim,0);
+    for (var i=0; i<dim[0]; i++) {
+      for (var j=0; j<dim[1]; j++) {
+        out[i][j] = arr[i][j];
       }
     }
-    console.log(fjac)
+    return out;
+  };
+
+  this.jacobian = function(params) {
+    var h, 
+        fjac = numeric.rep([self.xvals.length, self.npars],0),
+        origParams, 
+        modParams, 
+        fjac_row = [];
+
+    for (var i=0; i<params.length; i++) {
+      modParams = params.slice(0);
+      h = params[i] * self.epsilon;
+      modParams[i] = params[i] + h;
+      for (var j=0; j<self.xvals.length; j++) {
+        var val1 = self.model(self.xvals[j], modParams);
+        var val2 = self.model(self.xvals[j], params);
+        fjac[j][i] = (val1 - val2) / h;
+      }
+    }
+    if (self.fitterOptions.debug) {
+      console.log("fjac:");
+      console.log(numeric.prettyPrint(fjac))
+    }
     return fjac;
   };
 
   this.iterate = function (params) {
-    //newParams = oldParams + (Jt * J)^-1 * Jt * residuals
+    //newParams = oldParams - [Jt•J - lam*diag(Jt•J)]^-1 • Jt•R
     var jac = self.jacobian(params);
     var jacTrans = numeric.transpose(jac);
-    var step1, step2, step3, newParams;
+    var jtj = numeric.dot(jacTrans, jac);
+    var diag = self.diagonal(jtj);
+    var step1, step2, step3, step4,
+        term2, lambda, newParams;
 
-    step1 = numeric.inv(numeric.dot(jacTrans, jac));
-    step2 = numeric.dot(step1, jacTrans);
-    step3 = numeric.dot(step2, self.residuals);
-
-    newParams = numeric.add(params, step3);
+    lambda = 0.001;
+    // console.log( numeric.prettyPrint(numeric.dot(jacTrans, jac) ))
+    term2 = numeric.mul(jtj, lambda)
+    console.log(term2)
+    step2 = numeric.inv(numeric.sub(jtj, term2))
+    console.log(step2)
+    step3 = numeric.dot(step2, jacTrans);
+    console.log(step3)
+    // console.log("step2", numeric.prettyPrint(step2))
+    step4 = numeric.dot(step3, self.residuals(params));
+    // console.log("params", numeric.prettyPrint(params))
+    // console.log("residuals", numeric.prettyPrint(self.residuals(params)))
+    // console.log("step3", numeric.prettyPrint(step3));
+    newParams = numeric.sub(params, step3);
     return newParams;
   };
 
 
   this.fit = function() {
     var iterationNumber = 0, 
-        paramEstimate = self.params;
+        paramEstimate = self.params,
+        oldParams,
+        oldSSR,
+        converge,
+        ssr = self.ssr(paramEstimate);
 
-    for (var i=0; i<=fitterOptions.maxIterations; i++) {
-      paramEstimate = self.iterate(paramEstimate);
-      console.log(paramEstimate);
+    for (var i=0; i<=self.fitterOptions.maxIterations; i++) {
+      iterationNumber++
+      oldParams = paramEstimate;
+      oldSSR = self.ssr(paramEstimate);
+      paramEstimate = self.iterate(oldParams);
+      ssr = self.ssr(paramEstimate)
+
+      converge = Math.abs((ssr-oldSSR)/ssr);
+      if (self.fitterOptions.debug) {
+        console.log("parestimate", paramEstimate, converge);
+      }
+
+      if (converge < 0.0001) {
+        paramEstimate = oldParams;
+        ssr = oldSSR;
+        break;
+      }
+
     }
+    return {"params": paramEstimate, "ssr": ssr, iterations:iterationNumber};
   };
 }
--- test/test.js
+++ test/test.js
@@ -1,19 +1,30 @@
-var linear = function(x, params) {
+function exponential(x, params) {
+  var A = params[0], 
+      d = params[1];
+  return (A * Math.exp(d * x));
+}
+
+function linear(x, params) {
   var m = params[0], 
       b = params[1];
   return (m*x + b);
-};
+}
+
+
 
 var data = [
-        [1, 2.5, 3, 4, 5], 
-        [1, 2.5, 3, 4, 5]
+        [1, 2, 3, 4, 5], 
+        [1, 2.1, 3.2, 4.0, 5.3]
        ];
 
-var p0 = [1.2, 2.0];
+var p0 = [1.1, 0.1];
 
-console.log(linear, data, p0)
-var minimizer = new Minimizer(linear, data, p0);
-minimizer.fit();
+var minimizer = new Minimizer(linear, data, p0, {'debug': true, parInfo: [1, 2]});
+var start = new Date().getTime();
+var fit = minimizer.fit();
+var end = new Date().getTime();
+var time = end - start;
+console.log("time", time)
 
 
 
