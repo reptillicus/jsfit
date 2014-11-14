6d294645101993244401adba3366b181b1776158
Joe Meiring <josephmeiring@gmail.com>
2014-11-14 12:34:40 -0600

    gaussian

--- app/app.js
+++ app/app.js
@@ -107,6 +107,16 @@ function sine(x, params) {
   return C + A * Math.sin(w*x);
 }
 
+function gaussian(x, params) {
+  C = params[0];
+  A = params[1];
+  mu = params[2];
+  sig = params[3];
+
+  return C + A*Math.exp(-(Math.pow(x - mu, 2))/(2 * Math.pow(sig, 2)));
+
+}
+
 
 var generatePointsData = function(data, model, p0, params) {
   var points = [],
--- app/controllers.js
+++ app/controllers.js
@@ -33,10 +33,10 @@ app.controller('example1Ctrl', function($scope) {
 
 app.controller('example2Ctrl', function($scope) {
 
-  p0 = [0.0, 10.0, 1.0];
-  var npoints = 100;
+  p0 = [20, 100.0, 5.0, 1.0];
+  var npoints = 200;
   xvals = numeric.linspace(0,10, npoints);
-  clean = xvals.map(function(d, i){return sine(d, p0);});
+  clean = xvals.map(function(d, i){return gaussian(d, p0);});
   noise = numeric.add(numeric.sub(numeric.mul(numeric.random([npoints]), 0.1), 0.05), 1.0);
   yvals = numeric.mul(clean, noise);
   // console.log(weights)
@@ -46,17 +46,17 @@ app.controller('example2Ctrl', function($scope) {
           ];
   $scope.xvals = $scope.data[0];
   $scope.yvals = $scope.data[1];
-  $scope.weights = $scope.data[2];
+  // $scope.weights = $scope.data[2];
 
-  $scope.p0 = [ {value:2.0}, {value:6.0}, {value:1.3} ];
-  $scope.parInfo = [{'name': 'C', 'fixed':false}, {'name': 'A', 'fixed':true}, {'name': 'w'}];
+  $scope.p0 = [ {value:20.0}, {value:80.0}, {value:4.3}, {value:0.5} ];
+  $scope.parInfo = [{'name': 'C', 'fixed':false}, {'name': 'A', 'fixed':false}, {'name': 'mu'}, {'name':'sigma', 'fixed':false }];
   
 
   $scope.fit = function() {
     par0 = $scope.p0.map(function(p) {return p.value});
-    var minimizer = new Minimizer(sine, $scope.data, par0, {'debug': false, parInfo: $scope.parInfo });
+    var minimizer = new Minimizer(gaussian, $scope.data, par0, {'debug': false, parInfo: $scope.parInfo });
     $scope.fitobj = minimizer.fit();
-    createChart({"data":generatePointsData($scope.data, sine, par0, $scope.fitobj.params), element:"#chart2"});
+    createChart({"data":generatePointsData($scope.data, gaussian, par0, $scope.fitobj.params), element:"#chart2"});
   };
 
   $scope.fit();
